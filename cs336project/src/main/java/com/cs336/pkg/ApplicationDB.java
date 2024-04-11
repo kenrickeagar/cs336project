70acf50dff3a95720baa3fd2f4297aee92197a28
kenrickeagar <125171930+kenrickeagar@users.noreply.github.com>
2024-04-10 23:33:04 -0400

    add comment

--- cs336project/src/main/java/com/cs336/pkg/ApplicationDB.java
+++ cs336project/src/main/java/com/cs336/pkg/ApplicationDB.java
@@ -31,7 +31,7 @@ public class ApplicationDB {
 		}
 		try {
 			//Create a connection to your DB
-			connection = DriverManager.getConnection(connectionUrl,"root","Current-Root-Password");
+			connection = DriverManager.getConnection(connectionUrl,"root","root"); //enter your DB password in the second third field
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
