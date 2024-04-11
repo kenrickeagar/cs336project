ca7e3d48d0ac515daee2b1c7fcf0c79ff86db2b4
kenrickeagar <125171930+kenrickeagar@users.noreply.github.com>
2024-04-10 23:29:52 -0400

    add item queries

--- cs336-login-database.sql
+++ cs336-login-database.sql
@@ -2,6 +2,11 @@ use cs336project;
 
 CREATE TABLE Users (id int AUTO_INCREMENT , username varchar(25), password varchar(25), PRIMARY KEY (id));
 
+#Uncomment below if testing to see items page
+#CREATE TABLE Items (i_id int AUTO_INCREMENT, item_name varchar(25), unit_price float, description varchar(100), PRIMARY KEY (i_id));
+#INSERT INTO Items (item_name, unit_price) VALUES ('RED SHIRT', 12.00);
+#INSERT INTO Items (item_name, unit_price) VALUES ('BLUE SHIRT', 40.63);
+
 INSERT INTO Users (username, password)
 VALUES ('zach', 'password123');
 
