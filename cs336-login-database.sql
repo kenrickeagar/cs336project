4508dce8c3d2d0cafb53851e929aebd5f4cdea18
kenrickeagar <125171930+kenrickeagar@users.noreply.github.com>
2024-04-17 19:05:39 -0400

    Update cs336-login-database.sql

--- cs336-login-database.sql
+++ cs336-login-database.sql
@@ -4,7 +4,7 @@ use cs336project;
 
 CREATE TABLE Users (id int AUTO_INCREMENT , username varchar(25), password varchar(25), PRIMARY KEY (id));
 
-CREATE TABLE Items (i_id int AUTO_INCREMENT, item_name varchar(25), unit_price float, description varchar(100), cid varchar(20), foreign key (cid) references Category (cid), PRIMARY KEY (i_id));
+CREATE TABLE Items (i_id int AUTO_INCREMENT, item_name varchar(25), unit_price float, description varchar(100), cid varchar(20), foreign key (cid) references Category (cid), subcatAttribute varchar(20), PRIMARY KEY (i_id));
 CREATE TABLE Category(cid varchar(20), cname varchar(20), i_id int, PRIMARY KEY (cid));
 INSERT INTO Items (item_name, unit_price) VALUES ('RED SHIRT', 12.00);
 INSERT INTO Items (item_name, unit_price) VALUES ('BLUE SHIRT', 40.63);
