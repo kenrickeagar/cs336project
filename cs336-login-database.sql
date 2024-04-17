6395f085ca12186b536f45f3764463338010065f
kenrickeagar <125171930+kenrickeagar@users.noreply.github.com>
2024-04-17 14:13:40 -0400

    Update cs336-login-database.sql

--- cs336-login-database.sql
+++ cs336-login-database.sql
@@ -4,11 +4,10 @@ use cs336project;
 
 CREATE TABLE Users (id int AUTO_INCREMENT , username varchar(25), password varchar(25), PRIMARY KEY (id));
 
-#Uncomment below if testing to see items page
-#CREATE TABLE Items (i_id int AUTO_INCREMENT, item_name varchar(25), unit_price float, description varchar(100), cid varchar(20), foreign key (cid) references Category (cid), PRIMARY KEY (i_id));
-#CREATE TABLE Category(cid varchar(20), cname varchar(20), i_id int, PRIMARY KEY (cid));
-#INSERT INTO Items (item_name, unit_price) VALUES ('RED SHIRT', 12.00);
-#INSERT INTO Items (item_name, unit_price) VALUES ('BLUE SHIRT', 40.63);
+CREATE TABLE Items (i_id int AUTO_INCREMENT, item_name varchar(25), unit_price float, description varchar(100), cid varchar(20), foreign key (cid) references Category (cid), PRIMARY KEY (i_id));
+CREATE TABLE Category(cid varchar(20), cname varchar(20), i_id int, PRIMARY KEY (cid));
+INSERT INTO Items (item_name, unit_price) VALUES ('RED SHIRT', 12.00);
+INSERT INTO Items (item_name, unit_price) VALUES ('BLUE SHIRT', 40.63);
 
 INSERT INTO Users (username, password)
 VALUES ('zach', 'password123');
