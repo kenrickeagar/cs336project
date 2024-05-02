a269a19913fd6450fb0be72eb46c320c69f6275b
kenrickeagar <125171930+kenrickeagar@users.noreply.github.com>
2024-05-01 21:25:04 -0400

    Add delete + Update Cascade to Foreign keys

--- cs336-login-database.sql
+++ cs336-login-database.sql
@@ -4,11 +4,11 @@ use cs336project;
 
 CREATE TABLE Users (id int AUTO_INCREMENT , username varchar(25), password varchar(25), PRIMARY KEY (id));
 CREATE TABLE Category(cid varchar(20), cname varchar(20), PRIMARY KEY (cid));
-CREATE TABLE Items (i_id int AUTO_INCREMENT, item_name varchar(25), unit_price float, min_price float, description varchar(100), cid varchar(20), foreign key (cid) references Category (cid), subcatAttribute varchar(20), closing_date_time datetime,seller_id int,winner_id int, foreign key(seller_id) references Users (id),foreign key(winner_id) references Users (id), PRIMARY KEY (i_id));
-CREATE TABLE Bids(amount int, time_of_bid datetime, buyer_id int, i_id int, foreign key (buyer_id) references users (id), foreign key (i_id) references items (i_id));
-CREATE TABLE Questions(question varchar(100), answer varchar(100), id int, foreign key (id) references Users (id));
-CREATE TABLE Auto_Bids(ab_id int AUTO_INCREMENT,id int, i_id int, prev_price float, amount float, increment float, foreign key (id) references Users (id),foreign key (i_id) references Items (i_id),primary key(ab_id));
-CREATE TABLE Item_Alert(alert_id int AUTO_INCREMENT, id int, item_description varchar(25), foreign key (id) references users (id), primary key (alert_id));
+CREATE TABLE Items (i_id int AUTO_INCREMENT, item_name varchar(25), unit_price float, min_price float, description varchar(100), cid varchar(20), foreign key (cid) references Category (cid), subcatAttribute varchar(20), closing_date_time datetime,seller_id int,winner_id int, foreign key(seller_id) references Users (id)ON DELETE CASCADE ON UPDATE CASCADE,foreign key(winner_id) references Users (id) ON DELETE CASCADE ON UPDATE CASCADE, PRIMARY KEY (i_id));
+CREATE TABLE Bids(amount int, time_of_bid datetime, buyer_id int, i_id int, foreign key (buyer_id) references users (id) ON DELETE CASCADE ON UPDATE CASCADE, foreign key (i_id) references items (i_id) ON DELETE CASCADE ON UPDATE CASCADE);
+CREATE TABLE Questions(question varchar(100), answer varchar(100), id int, foreign key (id) references Users (id) ON DELETE CASCADE ON UPDATE CASCADE);
+CREATE TABLE Auto_Bids(ab_id int AUTO_INCREMENT,id int, i_id int, prev_price float, amount float, increment float, foreign key (id) references Users (id) ON DELETE CASCADE ON UPDATE CASCADE, foreign key (i_id) references Items (i_id) ON DELETE CASCADE ON UPDATE CASCADE,primary key(ab_id));
+CREATE TABLE Item_Alert(alert_id int AUTO_INCREMENT, id int, item_description varchar(25), foreign key (id) references users (id) ON DELETE CASCADE ON UPDATE CASCADE, primary key (alert_id));
 CREATE TABLE Admin(aid int AUTO_INCREMENT, username varchar(25), password varchar(25), PRIMARY KEY (aid));
 CREATE TABLE Customer_Reps(cr_id int AUTO_INCREMENT, username varchar(25), aid int, password varchar(25),foreign key (aid) references Admin (aid), primary key (cr_id));
 
